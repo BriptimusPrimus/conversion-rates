c0e6c9790c40f2f356fca97e39dff2df48dea6ac
briptimus <brituor@gmail.com>
2015-06-15 19:18:46 -0700

    added functionality of reading from a file and parsing it iinto a rates object

--- server/ratessources/localFileStorage.js
+++ server/ratessources/localFileStorage.js
@@ -3,11 +3,43 @@
 // 	function getMap()
 // 	function updateRates(updates)
 
-// "rates.txt"
+var _path = "rates.txt";
+var fs = require('fs');
+
+function parseArrayToRatesMap(arr){
+	var result = {};
+	var code, sym, val;
+	for(k in arr){
+		code = arr[k].slice(0,3); 
+		sym = arr[k].slice(4,5); 
+		rate = arr[k].slice(6); 
+		result[code] = { 
+			currencyCode : code,
+			symbol : sym,
+			rate : val			
+		}
+	}
+	return result;	
+}
+
+function readFileContent(callback) {
+    fs.readFile(_path, "utf-8", function (err, content) {
+        if (err) 
+        	return callback(err);
+        callback(null, content);
+    });
+}
 
 function readConversionRates(){
 	// TODO
-	return {};
+	readFileContent(function(err, content){
+		if(err){
+			console.log("Error trying to read file "+err.message);
+			throw err; 
+		}
+		var arr = content.split('\n');
+		return parseArrayToRatesMap(arr);
+	});
 }
 
 function updateConversionRates(updates){
