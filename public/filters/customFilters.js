3e998826df43cf8b556182d6446ef5785b2787ba
briptimus <brituor@gmail.com>
2015-06-17 16:11:50 -0700

    added client tests for the filters

--- public/filters/customFilters.js
+++ public/filters/customFilters.js
@@ -1,6 +1,6 @@
 angular.module("customFilters", [])
     .filter("capitalize", function() {
-        return function(input, scope) {
+        return function(input) {
         	if (angular.isString(input)) {
 	            input = input.toLowerCase();
 	            return input.substring(0,1).toUpperCase()+input.substring(1);        		
