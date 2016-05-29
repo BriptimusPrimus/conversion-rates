f64db92a83ff0593d3b02ab45cd704543beac7bb
briptimus <brituor@gmail.com>
2016-05-29 06:33:36 -0700

    Settiing hardcoded cookie in the client to assist hackaton project, allowing cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //server.js
 
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+
+app.use(cookieParser('my secret here'));
 
 var config = require('./config.js');
 var curConv = require('./server/currencyConversion.js');
@@ -13,6 +16,15 @@ curConv.setConfiguration({
 //static files location
 app.use(express.static(__dirname + '/public'));
 
+// Allow CORS
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // /symbols
 app.get('/symbols', function(req, res){
     console.log("request to: "+req.url);
@@ -20,6 +32,7 @@ app.get('/symbols', function(req, res){
         if(err) 
             return res.status(500).end();
 
+        res.cookie('session_token', 'type_basic');
         res.json(symbolsMap);
     })
 });
