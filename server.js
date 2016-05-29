cb40eb8ace67a6caaceb13af9ac0f93732229e80
briptimus <brituor@gmail.com>
2016-05-29 06:33:36 -0700

    Settiing hardcoded cookie in the client to assist hackaton project, allowing cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 //server.js
 
 var express = require('express');
+var cookieParser = require('cookie-parser');
 var app = express();
+
+app.use(cookieParser('my secret here'));
 
 var config = require('./config.js');
 var curConv = require('./server/currencyConversion.js');
@@ -13,13 +16,24 @@ curConv.setConfiguration({
 //static files location
 app.use(express.static(__dirname + '/public'));
 
+// Allow CORS
+app.use(function(req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Credentials: true");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // /symbols
 app.get('/symbols', function(req, res){
     console.log("request to: "+req.url);
     curConv.getCurrencySymbols(function(err, symbolsMap){
-        if(err) 
+        if(err)
             return res.status(500).end();
 
+        res.cookie('session_token', 'type_basic');
         res.json(symbolsMap);
     })
 });
@@ -27,9 +41,9 @@ app.get('/symbols', function(req, res){
 // /paypal/conversionRate?convertFrom=EUR&convertTo=CAD
 app.get('/paypal/conversionRate', function(req, res){
     console.log("request to: "+req.url);
-    curConv.getConversionRate(req.query.convertFrom, req.query.convertTo, 
+    curConv.getConversionRate(req.query.convertFrom, req.query.convertTo,
         function(err, result){
-            if(err) 
+            if(err)
                 return res.status(500).end();
 
             res.json({conversionRate: result});
@@ -40,16 +54,17 @@ app.get('/paypal/conversionRate', function(req, res){
 app.get('/paypal/currencyConversion', function(req, res){
     console.log("request to: "+req.url);
 
-    curConv.convertFromAtoB(req.query.amount, req.query.convertFrom, 
+    curConv.convertFromAtoB(req.query.amount, req.query.convertFrom,
         req.query.convertTo, function(err, result){
             if(err)
                 return res.status(500).end();
 
             res.json(result);
-        });   
+        });
 })
 
 
-app.listen(3000, function(){
-	console.log("Server App listening on port 3000");
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+	console.log("Server App listening on port ", port);
 });
